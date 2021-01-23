82c16cc5e9382fd541153a313af0d32e39899600
Marco Moroni <marcomoroni99@gmail.com>
2021-01-23 17:46:57 +0100

    initial commit

---
+++ src/main/java/MeT/server/MainServer.java
@@ -0,0 +1,42 @@
+package MeT.server;
+
+import MeT.server.resources.coap.CoapPirResource;
+import MeT.server.resources.raw.PirRawSensor;
+import MeT.server.resources.raw.ResourceDataListener;
+import MeT.server.resources.raw.SmartObjectResource;
+import org.eclipse.californium.core.CoapServer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class MainServer extends CoapServer{
+    private static Logger logger = LoggerFactory.getLogger(MainServer.class);
+
+    public MainServer(){
+        super();
+        String deviceId = String.format("dipi:iot:%s", UUID.randomUUID().toString());
+
+        PirRawSensor pirRawSensor = new PirRawSensor();
+        logger.info("ciao", pirRawSensor);
+        System.out.println("ciao");
+        CoapPirResource coapPirResource = new CoapPirResource(deviceId, "pir", pirRawSensor);
+
+        this.add(coapPirResource);
+
+        pirRawSensor.addDataListener(new ResourceDataListener<Date>() {
+            @Override
+            public void onDataChanged(SmartObjectResource<Date> resource, Date updatedValue) {
+                if(resource != null && updatedValue != null)
+                    logger.info("PIR Device: {} -> Ti sono entrati in casa, s'ta 'tenti - SERVER", resource.getId(), updatedValue);
+
+            }
+        });
+    }
+    public static void main(String[] args){
+        MainServer server = new MainServer();
+        server.start();
+        logger.info("server started on port {}", server.toString());
+    }
+}
---
+++ src/main/java/MeT/server/resources/coap/CoapPirResource.java
@@ -0,0 +1,81 @@
+package MeT.server.resources.coap;
+
+import MeT.server.resources.raw.PirRawSensor;
+import MeT.server.resources.raw.ResourceDataListener;
+import MeT.server.resources.raw.SmartObjectResource;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.eclipse.californium.core.coap.CoAP;
+
+import java.util.Date;
+
+public class CoapPirResource extends CoapResource {
+
+    private static Logger logger = LoggerFactory.getLogger(CoapPirResource.class);
+    private String deviceId;
+    private PirRawSensor pirRawSensor;
+    private static final String OBJECT_TITLE = "PIR Sensor";
+    private Date updateDate;
+
+    public CoapPirResource(String deviceId, String url, PirRawSensor pirRawSensor){
+        super(url);
+
+        if (deviceId == null || url == null || pirRawSensor == null) {
+            logger.error("Non va un cazzo");
+            return;
+        }
+
+        this.pirRawSensor = pirRawSensor;
+        this.deviceId = deviceId;
+
+        this.setObservable(true);
+        this.setObserveType(CoAP.Type.CON);
+        this.getAttributes().setTitle(OBJECT_TITLE);
+        this.getAttributes().setObservable();
+        this.getAttributes().addAttribute("rt", PirRawSensor.RESOURCE_TYPE);
+        this.getAttributes().addAttribute("ct", Integer.toString(MediaTypeRegistry.TEXT_PLAIN));
+
+        this.pirRawSensor.addDataListener(new ResourceDataListener<Date>() {
+            @Override
+            public void onDataChanged(SmartObjectResource<Date> resource, Date updatedValue) {
+                if(resource == null || updatedValue == null) return;
+                updateDate = updatedValue;
+                logger.info("PIR Device: {} -> Ti sono entrati in casa, s'ta 'tenti - Resource", resource.getId(), updatedValue);
+            }
+        });
+
+    }
+
+    public void handleGET(CoapExchange exchange){
+        if(!(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.TEXT_PLAIN))
+            exchange.respond(CoAP.ResponseCode.BAD_REQUEST, "Wrong content type");
+
+        exchange.respond(CoAP.ResponseCode.CONTENT, this.updateDate.toString());
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
